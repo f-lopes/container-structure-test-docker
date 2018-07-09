6441854ce6f90deafd821c1d6e77b16abd957142
Florian Lopes <florian.lopes@outlook.com>
2018-07-09 06:40:05 +0200

    Update test command

--- test/tests.sh
+++ test/tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-container-structure-test test -v DEBUG --image flopes/container-structure-test-docker --config tests_config.yml
+container-structure-test test -v DEBUG --image flopes/container-structure-test-docker --config test/tests_config.yml
